7595bbb126e5b22a9cd0c5fee88570ab672c9e60
liangchangyou <liangchangyoujackson@gmail.com>
2020-01-29 12:05:14 +0800

    attr

--- src/main/scala/org/jackson/chapter06/oop/ObjectDemo.scala
+++ src/main/scala/org/jackson/chapter06/oop/ObjectDemo.scala
@@ -5,6 +5,18 @@ package org.jackson.chapter06.oop
  * 1.类申明默认是public,不要写public
  * 2. 一个scala文件中，可以定义很多个类 class,都是public
  * 3. 类的属性，可以是值类型，也可以是引用类型
+ *
+ * 属性
+ *  1. [访问类型] var 属性名称[:类型]=属性
+ *  2. 值或引用
+ *  2. 必须初始默认值
+ *  3. 如果是null，注意
+ *  4. 默认值使用 _
+ *  5. 不同对象，互不影响
+ *
+ *
+ * 高级部分:
+ * 构造器
  */
 object ObjectDemo {
   def main(args: Array[String]): Unit = {
