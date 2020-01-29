26a05665715eb607af4e301d7f77c76f2e8cfe69
liangchangyou <liangchangyoujackson@gmail.com>
2020-01-29 16:07:01 +0800

    fun

--- src/main/scala/org/jackson/chapter06/oop/ObjectDemo.scala
+++ src/main/scala/org/jackson/chapter06/oop/ObjectDemo.scala
@@ -17,11 +17,52 @@ package org.jackson.chapter06.oop
  *
  * 高级部分:
  * 构造器
+ *
+ *
+ *
+ * 对象：
+ * val |var  name[:type] = new class()
+ * :type可以省略，但是如果父类，进行多态的时候，必须指定
+ *
+ * 类和对象的内存分配
+ *
  */
 object ObjectDemo {
   def main(args: Array[String]): Unit = {
 
+    val emp = new Emp
+    val emp1: Person = new Emp
+    println(emp)
+    println(emp1)
+
+    val n = 10
+    val res = (0 to n).reverse
+    println(res)
+    // 函数(f:Int => U),即接收一个输入参数为Int,输出为u的函数
+    // 将res遍历出来，将元素传给print
+    //高级函数 匿名函数 _ 表示参数
+    res.foreach((i) => {
+      println(i)
+    })
+
+    var sum: Long = 0
+    "hello".foreach({
+      sum += _.toLong
+    })
   }
+
+  def myshow(n: Int): Unit = {
+    println(n)
+  }
+}
+
+
+class Person {
+
+}
+
+class Emp extends Person {
+
 }
 
 
