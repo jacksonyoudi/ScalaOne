83f5cdb694a28f03abaf0077db07225d7e715f63
liangchangyou <liangchangyoujackson@gmail.com>
2020-02-02 00:26:50 +0800

    mix

---
+++ src/main/scala/org/jackson/chapter08/mixin/AddTraits.scala
@@ -0,0 +1,60 @@
+package org.jackson.chapter08.mixin
+
+/**
+ * 如果同时 混入多个特质，称之为 叠加特质
+ * 构建顺序
+ * 执行方法顺序
+ */
+object AddTraits {
+  def main(args: Array[String]): Unit = {
+    val sql = new MySql with DB with File
+    // 1. 动态混入，创建混入对象时， 顺序, 重复 就不继承了
+    // Operate data  db File
+    // scala叠加特质的时候，会首先从后面的特质开始执行(即从左到右)
+
+    // 执行混入对象方法的时候，顺序又是什么？
+    sql.insert(1)
+    // 顺序是 1. 从右到左开始执行，  2. 当执行到super时，是指的左边的特质
+    // 3. 如果左边没有特质，则super就是父特质
+    //
+    // 向文件 向数据库  插入数据
+  }
+}
+
+trait Operate4 {
+  println("Operate")
+
+  def insert(id: Int)
+}
+
+
+trait Data extends Operate4 {
+  println("data")
+
+  override def insert(id: Int): Unit = {
+    println("插入数据:", id)
+  }
+}
+
+
+trait DB extends Data {
+  println("db")
+
+  override def insert(id: Int): Unit = {
+    println("向数据库")
+    super.insert(id)
+  }
+}
+
+trait File extends Data {
+  println("File")
+
+  override def insert(id: Int): Unit = {
+    println("向文件")
+    super.insert(id) // 这里的super 在动态混入，和顺序有关
+  }
+}
+
+class MySql {
+
+}
---
+++ src/main/scala/org/jackson/chapter08/mixin/Demo01.scala
@@ -0,0 +1,53 @@
+package org.jackson.chapter08.mixin
+
+/**
+ * 开闭原则 对源码闭合 对功能扩展开放
+ *
+ *
+ * scala创建对象的形式
+ * 1. new
+ * 2. apply
+ * 3. 匿名子类
+ * 4. 动态混入
+ */
+object Demo01 {
+  def main(args: Array[String]): Unit = {
+    // 在不修改类的定义的基础，让他们可以使用trait方法
+    val b = new OracleDB with Operate
+    b.insert(10)
+
+    val b1 = new MySqlDB with Operate
+    b1.insert(20)
+
+    //new MySqlDB {} // 相当于匿名子类
+
+    // 如果抽象类中有抽象方法,如何混入
+    val sqlite = new Sqlite with Operate {
+      override def say(): Unit = {
+        println("say")
+      }
+    }
+
+    sqlite.insert(300)
+  }
+}
+
+
+trait Operate {
+  def insert(id: Int): Unit = {
+    println("插入数据", id)
+  }
+}
+
+class OracleDB {
+
+}
+
+abstract class MySqlDB {
+
+}
+
+abstract class Sqlite {
+  def say()
+}
+
