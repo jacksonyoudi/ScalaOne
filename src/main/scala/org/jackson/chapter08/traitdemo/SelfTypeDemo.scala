ac705e1d608e0ef99310554fd3b7f6a258340a5f
liangchangyou <liangchangyoujackson@gmail.com>
2020-02-03 23:57:00 +0800

    self type

--- src/main/scala/org/jackson/chapter08/traitdemo/SelfTypeDemo.scala
+++ src/main/scala/org/jackson/chapter08/traitdemo/SelfTypeDemo.scala
@@ -1,5 +1,30 @@
 package org.jackson.chapter08.traitdemo
 
 object SelfTypeDemo {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+/**
+ * Logger 就是自身类型特质，当这里做了自身类型后，那么
+ * 防止嵌套使用
+ */
+trait Logger {
+  // 明确告诉编辑器 我就是exception,如果没有这句话，下
+  // 面 getMessage不能调用
+  this: Exception =>
+  {
+    def log(): Unit = {
+      println(getMessage)
+    }
+  }
+}
+
+//class ConsoleOne extends Logger {
+//
+//}
+
+class Console extends Exception with Logger {
 
 }
