9c45030b6cce639681a3989465be74f5b22de1af
liangchangyou <liangchangyoujackson@gmail.com>
2020-02-04 10:14:56 +0800

    inner

--- src/main/scala/org/jackson/chapter08/innerclass/ScalaInnerClass.scala
+++ src/main/scala/org/jackson/chapter08/innerclass/ScalaInnerClass.scala
@@ -5,8 +5,8 @@ package org.jackson.chapter08.innerclass
  */
 object ScalaInnerClass {
   def main(args: Array[String]): Unit = {
-    val soutClass = new SoutClass()
-    val soutClass1 = new SoutClass()
+    val soutClass = new SoutClassOne()
+    val soutClass1 = new SoutClassOne()
 
     // 对象 内部类 的方式创建 这里语法可以看出scala中，默认情况下内部类实例
     // 和外部对象关联
@@ -20,11 +20,12 @@ object ScalaInnerClass {
   }
 }
 
-// 内部类
+// 内部类名.this.属性
 class SoutClass {
   // 定义属性
   var name = "hello"
   private var sal = 3000
+
 
   /**
    * 访问方式: 外部类名.this.属性名
@@ -35,6 +36,27 @@ class SoutClass {
       println("name=" + SoutClass.this.name + " sal=" + SoutClass.this.sal)
     }
   }
+
+}
+
+
+// 使用别名
+class SoutClassOne {
+  myouter => // 这里可以理解，外部类的别名 看做是外部类的一个实例
+
+  /**
+   * 访问方式: 外部类名.this.属性名
+   * 内部类当成外部类的一个实例
+   */
+  class SInnerClass {
+    def info(): Unit = {
+      println("name=" + myouter.name + " sal=" + myouter.sal)
+    }
+  }
+
+  // 定义属性
+  var name = "jack"
+  private var sal = 3000
 
 }
 
