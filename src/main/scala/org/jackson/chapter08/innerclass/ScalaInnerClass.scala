6025d1eaecbf743b2c4456a8a2e56010ba1e4d7f
liangchangyou <liangchangyoujackson@gmail.com>
2020-02-04 10:07:56 +0800

    scala inner

--- src/main/scala/org/jackson/chapter08/innerclass/ScalaInnerClass.scala
+++ src/main/scala/org/jackson/chapter08/innerclass/ScalaInnerClass.scala
@@ -11,6 +11,7 @@ object ScalaInnerClass {
     // 对象 内部类 的方式创建 这里语法可以看出scala中，默认情况下内部类实例
     // 和外部对象关联
     val sInnerClass = new soutClass.SInnerClass()
+    sInnerClass.info()
     val sInnerClass1 = new soutClass1.SInnerClass()
 
 
@@ -21,9 +22,18 @@ object ScalaInnerClass {
 
 // 内部类
 class SoutClass {
+  // 定义属性
+  var name = "hello"
+  private var sal = 3000
 
+  /**
+   * 访问方式: 外部类名.this.属性名
+   * 内部类当成外部类的一个实例
+   */
   class SInnerClass {
-
+    def info(): Unit = {
+      println("name=" + SoutClass.this.name + " sal=" + SoutClass.this.sal)
+    }
   }
 
 }
