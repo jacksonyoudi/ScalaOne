4ed3e46c6e2efd88e88c770b42a8e65e62b1dd5f
liangchangyou <liangchangyoujackson@gmail.com>
2020-02-06 23:42:07 +0800

    array

---
+++ src/main/scala/org/jackson/chapter10/ArrayDemo01.scala
@@ -0,0 +1,29 @@
+package org.jackson.chapter10
+
+object ArrayDemo01 {
+  def main(args: Array[String]): Unit = {
+
+    // 泛型 [any]任意类型
+    /**
+     * 没有赋值，默认是0
+     * array是不可变类型
+     * 修改通过索引可以直接操作
+     *
+     */
+    val ints = new Array[Int](4)
+
+    println(ints.length)
+    for (elem <- ints) {
+      println(elem)
+    }
+
+    ints(3) = 10
+    for (elem <- ints) {
+      println(elem)
+    }
+
+  }
+}
+
+
+
