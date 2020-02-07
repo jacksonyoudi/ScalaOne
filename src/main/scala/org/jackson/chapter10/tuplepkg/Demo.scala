3ab794dc00213bfa94ccc841b990776b740c562f
liangchangyou <liangchangyoujackson@gmail.com>
2020-02-07 11:31:50 +0800

    tuple

---
+++ src/main/scala/org/jackson/chapter10/tuplepkg/Demo.scala
@@ -0,0 +1,18 @@
+package org.jackson.chapter10.tuplepkg
+
+/**
+ * 容器
+ * 灵活，将多个无关的数据封装为一个整体 对数据没有过多的约束
+ *
+ * 注意： 元祖中最大只能有22个元素
+ *
+ */
+object Demo {
+  def main(args: Array[String]): Unit = {
+    // 说明 1. tuple1 就是一个tuple 类型就是tuple4
+    // 简单说明： 为了高效的操作元祖，编译器根据元素的个数不同，对应不同的元祖类型
+    // 分别 Tuple1---> Tuple22
+    val tuple1 = (1, 2, 3, 4, "hello", 4)
+    println(tuple1)
+  }
+}
